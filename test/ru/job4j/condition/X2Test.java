aef9c061513743c1e8e1ead3e7c112f27f680b1f
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-08-21 22:31:22 +0300

    1.2. Входные данные в тесте [#297750]

--- test/ru/job4j/condition/X2Test.java
+++ test/ru/job4j/condition/X2Test.java
@@ -19,7 +19,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X1Then4() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -52,7 +52,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X0Then() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
