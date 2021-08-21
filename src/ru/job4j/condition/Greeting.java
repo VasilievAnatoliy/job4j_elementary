50a68d42eb2def9e92cefa0ac8603e43cb9d2c92
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-08-21 16:45:45 +0300

    9.1. String [#237407 #191951]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idia = "I like Java!";
+        System.out.println(idia);
+        idia += " But I am a newbie. ";
+        int year = 2021;
+        idia += year;
+        System.out.println(idia);
+    }
+}
+
--- src/ru/job4j/ru/job4j/condition/Greeting.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.ru.job4j.condition;
-
-public class Greeting {
-    public static void main(String[] args) {
-        int year = 2021;
-        String idia = "I like Java!" + " "+"But I am a newbie." + " " +year;
-        System.out.println(idia);
-    }
-}
