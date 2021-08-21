ba71c9513fb9e1925692de128a7dd3d8167b696a
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-08-21 23:11:58 +0300

    Входные данные в тесте [#297750]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idia = "I like Java!";
-        System.out.println(idia);
-        idia += " But I am a newbie. ";
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
         int year = 2021;
-        idia += year;
-        System.out.println(idia);
+        idea += year;
+        System.out.println(idea);
     }
 }
 
