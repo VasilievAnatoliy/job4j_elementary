0e16224025b075ec5b89564730b583f987b1a1fc
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-08-30 12:28:03 +0300

    5.8. Именование классов и переменных [#4087]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double x = Math.pow((x2 - x1),2);
-        double y = Math.pow((y2 - y1),2);
+        double x = Math.pow((x2 - x1), 2);
+        double y = Math.pow((y2 - y1), 2);
         double rsl = Math.sqrt(x + y);
         return rsl;
     }
