aa96e3fd990fa236d6195a7dc37a51b2d792cc82
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2021-09-03 09:29:36 +0300

    5.7. Ипотека [#156315 #196561]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -6,9 +6,6 @@ public class Mortgage {
         while (amount > 0) {
             amount += amount * (percent / 100) - salary;
             year++;
-            if (amount < 0) {
-                break;
-            }
         }
         return year;
     }
